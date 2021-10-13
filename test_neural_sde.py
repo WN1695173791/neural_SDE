cc760c03748709dd46be20f21ff8eb567e813d9c
Paalis <pls19961219@gmail.com>
2021-10-13 15:03:47 -0400

    init training

--- test_neural_sde.py
+++ test_neural_sde.py
@@ -1,7 +1,10 @@
 import torch
 import torchsde
+import matplotlib.pyplot as plt
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-batch_size, state_size, brownian_size = 32, 3, 2
+batch_size, state_size, brownian_size = 100, 1, 2
 t_size = 20
 
 class SDE(torch.nn.Module):
@@ -31,3 +34,10 @@ ts = torch.linspace(0, 1, t_size)
 # Initial state y0, the SDE is solved over the interval [ts[0], ts[-1]].
 # ys will have shape (t_size, batch_size, state_size)
 ys = torchsde.sdeint(sde, y0, ts)
+
+print(ys.shape)
+
+for i in range(ys.shape[1]):
+    plt.plot(ys[:, i, :].detach().numpy())
+plt.show()
+
